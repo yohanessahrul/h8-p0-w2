c77444ee5f73af61c3d664a155eed5bf121a81b6
yosaid92 <yohanessahrul92@gmail.com>
2017-12-19 09:34:02 +0700

    add exercise-5 on week 2, for

---
+++ exercise-5.js
@@ -0,0 +1,12 @@
+console.log('LOOPING PERTAMA');
+
+for(var angka1 = 1; angka1 <= 20; angka1++){
+  console.log(angka1 + '-' + ' I Love Coding');
+}
+
+
+console.log('LOOPING KEDUA');
+
+for(var angka2 = 20; angka2 >= 1; angka2--){
+  console.log(angka2 + '-' + 'I will become fullstack developer');
+}
