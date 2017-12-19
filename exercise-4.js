bf83aadb07013a5718e1f6381b9f8d57f582b8f0
yosaid92 <yohanessahrul92@gmail.com>
2017-12-19 09:19:30 +0700

    add exercise 4

---
+++ exercise-4.js
@@ -0,0 +1,24 @@
+console.log('LOOPING PERTAMA');
+
+var angka1 = 2;
+
+while(angka1 <= 20){
+  console.log(angka1 + ' I Love Coding')
+  angka1 += 2;
+}
+
+
+
+console.log('LOOPING KEDUA');
+
+var angka2 = 20;
+
+while(angka2 >= 1){
+  console.log(angka2 + ' I will become fullstack developer')
+  angka2 -= 2;
+}
+
+
+
+
+
