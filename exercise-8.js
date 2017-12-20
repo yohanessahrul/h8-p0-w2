aa30550c06f6c23c6708b5f378f51857405bd5ab
yosaid92 <yohanessahrul92@gmail.com>
2017-12-20 16:12:56 +0700

    add exercise 7(pseudocode) and 8(function)

---
+++ exercise-8.js
@@ -0,0 +1,19 @@
+// Tugas 1
+function shoutOut(){
+    return "Hallo World!";
+}
+console.log(shoutOut());
+
+// Tugas 2
+function calculateMultiply(num1, num2){
+  var hasil = num1 * num2;
+  return hasil;
+}
+console.log(calculateMultiply(5,6));
+
+// Tugas 3
+function processSentence(nama, age, address, hoby){
+  var fullSentences = "Nama saya " + nama + ", umur saya " + age + ", alamat saya di " + address + ", dan saya punya hobby yaitu " + hoby ;
+  return fullSentences;
+}
+console.log(processSentence("Yosa", "25 tahun", "Bekasi", "Bermain drum"));
