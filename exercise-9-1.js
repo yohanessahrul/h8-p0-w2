97ce46210c600e16e4ed9700c15bedc9800db386
yosaid92 <yohanessahrul92@gmail.com>
2017-12-20 16:49:49 +0700

    add Exercise 9 - Part 1. Pseudocode to Javascript Code

---
+++ exercise-9-1.js
@@ -0,0 +1,7 @@
+//Exercise 9 - Part 1
+var nama = "Yosa";
+var pembilang = 100;
+var penyebut = 20;
+var hasilBagi = pembilang / penyebut;
+
+console.log("Halo " + nama + ", " + pembilang + " dibagi " + penyebut + " adalah sama dengan " + hasilBagi);
